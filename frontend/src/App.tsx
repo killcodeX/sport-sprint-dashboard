625e8213fd9acf4d819bd379c6c8ca3691eed2fe
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-12 00:12:59 +0530

    initial project setup 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,16 @@
-import React from 'react';
-import { useState } from 'react'
-import './App.css';
+import React from "react";
+import Layout from "./layouts/components/aside";
+import Home from "./modules/home";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const handleIncrement = () => {
-    const newCount = count + 1
-    setCount(newCount)
-  }
-
-  const handleDecrement = () => {
-    const newCount = count - 1
-    setCount(newCount)
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-      <button className="btn btn-danger" data-cy="decrement" onClick={handleDecrement}>
-        -
-      </button>
-      <span className='mx-5' data-cy="counter">{count}</span>
-      <button className="btn btn-primary" data-cy="increment" onClick={handleIncrement}>
-        +
-      </button>
-      </header>
-    </div>
+    <>
+      {" "}
+      <Layout />
+      <Home />
+    </>
   );
 }
 

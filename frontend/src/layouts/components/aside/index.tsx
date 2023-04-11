9df682a8c249dc52e5a07b5a5b8211f62b7ca50a
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-12 02:03:28 +0530

    closing aside menu done

--- frontend/src/layouts/components/aside/index.tsx
+++ frontend/src/layouts/components/aside/index.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 
 export default function Aside() {
+  const [asideClose, setAsideClose] = useState(false);
+  const handleAsideClose = () => {
+    if (asideClose) {
+      document.documentElement.style.setProperty(
+        "--appside-bar-width",
+        "15rem"
+      );
+    } else {
+      document.documentElement.style.setProperty("--appside-bar-width", "5rem");
+    }
+    setAsideClose(!asideClose);
+  };
+
   return (
-    <div className='aside'>
-        <div className='container'>
-            <div className='logo'>Sports Stride</div>
-            <div className='aside-close btn btn-color-muted shadow-lg'>{'<<'}</div>
-            <div className='horizontal-line'></div>
-        </div>
+    <div className="aside">
+      <div className="container">
+        {asideClose ? (
+          <div className="logo">S</div>
+        ) : (
+          <div className="logo">Sports Stride</div>
+        )}
+        {asideClose ? (
+          <div
+            className="aside-close btn btn-icon btn-color-muted shadow-lg"
+            onClick={handleAsideClose}
+          >
+            <BsChevronDoubleRight />
+          </div>
+        ) : (
+          <div
+            className="aside-close btn btn-icon btn-color-muted shadow-lg"
+            onClick={handleAsideClose}
+          >
+            <BsChevronDoubleLeft />
+          </div>
+        )}
+        <div className="horizontal-line"></div>
+      </div>
     </div>
-  )
+  );
 }

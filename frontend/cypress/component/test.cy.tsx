88600a1c0ffae676be53e094ef11aa34d46c20d0
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-11 23:56:45 +0530

    updated app name

--- frontend/cypress/component/test.cy.tsx
+++ frontend/cypress/component/test.cy.tsx
@@ -1,8 +1,23 @@
-import 'jest';
+import React from "react";
+import Stepper from "../../src/App";
 
+describe("<Stepper />", () => {
+  it("renders", () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Stepper />);
+    cy.get("span").should("have.text", "0");
+  });
 
-describe('test.cy.tsx', () => {
-  it('playground', () => {
-    // cy.mount()
-  })
-})
+  it("when the increment button is pressed, the counter is incremented", () => {
+    cy.mount(<Stepper />);
+    cy.get("[data-cy=increment]").click();
+    cy.get("[data-cy=counter]").should("have.text", "1");
+  });
+
+  it("when the decrement button is pressed, the counter is decremented", () => {
+    cy.mount(<Stepper />);
+    cy.get("[data-cy=decrement]").click();
+    cy.get("[data-cy=counter]").should("have.text", "-1");
+  });
+  
+});
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const handleIncrement = () => {
+    const newCount = count + 1
+    setCount(newCount)
+  }
+
+  const handleDecrement = () => {
+    const newCount = count - 1
+    setCount(newCount)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <button type="button" className="btn btn-primary">Primary</button>
+      <button className="btn btn-danger" data-cy="decrement" onClick={handleDecrement}>
+        -
+      </button>
+      <span className='mx-5' data-cy="counter">{count}</span>
+      <button className="btn btn-primary" data-cy="increment" onClick={handleIncrement}>
+        +
+      </button>
       </header>
     </div>
   );

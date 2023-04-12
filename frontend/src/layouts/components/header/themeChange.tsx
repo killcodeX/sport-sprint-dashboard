8b1f46d544889bdd2a11747c62bb95aff9d33e38
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-12 23:57:20 +0530

    chnage theme is working

--- frontend/src/layouts/components/header/themeChange.tsx
+++ frontend/src/layouts/components/header/themeChange.tsx
@@ -4,14 +4,18 @@ import { HiOutlineComputerDesktop } from "react-icons/hi2";
 
 export default function ThemeChange() {
   const [theme, setTheme] = useState("light");
-  const [showOptions, setShowOptions] = useState(true);
 
   const handleThemeChange = (pref: string) => {
     if (pref === "light") {
+      document.documentElement.setAttribute("data-theme", "light");
       setTheme("light");
     } else if (pref === "dark") {
+      document.documentElement.setAttribute("data-theme", "dark");
       setTheme("dark");
     } else if (pref === "system") {
+      const prefersDark =
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches;
       setTheme("system");
     }
   };
@@ -20,37 +24,35 @@ export default function ThemeChange() {
       <div className="current-theme btn btn-active-light-primary">
         {theme === "light" ? <BsSun /> : <BsMoon />}
       </div>
-      {showOptions ? (
-        <div className="theme-options">
-          <div
-            className={`theme-option ${theme === "light" ? "active" : null}`}
-            onClick={() => handleThemeChange("light")}
-          >
-            <span className="icon">
-              <BsSun />
-            </span>{" "}
-            Light
-          </div>
-          <div
-            className={`theme-option ${theme === "dark" ? "active" : null}`}
-            onClick={() => handleThemeChange("dark")}
-          >
-            <span className="icon">
-              <BsMoon />
-            </span>{" "}
-            Dark
-          </div>
-          <div
-            className={`theme-option ${theme === "system" ? "active" : null}`}
-            onClick={() => handleThemeChange("system")}
-          >
-            <span className="icon">
-              <HiOutlineComputerDesktop />
-            </span>{" "}
-            System
-          </div>
+      <div className="theme-options">
+        <div
+          className={`theme-option ${theme === "light" ? "active" : null}`}
+          onClick={() => handleThemeChange("light")}
+        >
+          <span className="icon">
+            <BsSun />
+          </span>{" "}
+          Light
         </div>
-      ) : null}
+        <div
+          className={`theme-option ${theme === "dark" ? "active" : null}`}
+          onClick={() => handleThemeChange("dark")}
+        >
+          <span className="icon">
+            <BsMoon />
+          </span>{" "}
+          Dark
+        </div>
+        <div
+          className={`theme-option ${theme === "system" ? "active" : null}`}
+          onClick={() => handleThemeChange("system")}
+        >
+          <span className="icon">
+            <HiOutlineComputerDesktop />
+          </span>{" "}
+          System
+        </div>
+      </div>
     </div>
   );
 }

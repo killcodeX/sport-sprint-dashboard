a32f648fff6f9d37c691f72d243c735897c74058
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-04-14 02:06:43 +0530

    react-router installed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Layout from "./layouts";
+import About from "./modules/about";
 import Home from "./modules/home";
 
 function App() {
   return (
-    <>
-      {" "}
+    <BrowserRouter>
       <Layout />
       <div className="app-wrapper">
-      <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
